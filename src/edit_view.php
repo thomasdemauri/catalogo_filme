7f069f8eb07862ef46d4427880e42790f792c753
RFL-C <115659945+RFL-C@users.noreply.github.com>
2025-06-26 17:03:00 -0300

    Update edit_view.php

--- src/edit_view.php
+++ src/edit_view.php
@@ -35,7 +35,7 @@ if ($row = $result->fetch_assoc()) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Cadastro de Filme</title>
+  <title>Edição</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 min-h-screen flex items-center justify-center px-4">
